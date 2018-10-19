d6b866f85ae42f0af42774bc665ff91bac5f369d
murayama <tasuku.murayama@pactera.com>
2018-10-19 23:30:20 +0900

    pwa新規作成

---
+++ ServiceWorker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", function(e) 
+{
+    console.log("serviceworker install");
+});
+
+self.addEventListener("activate",function(e)
+{
+    console.log("serviceworker install");
+});
+
+self.addEventListener("fetch",function(e)
+{
+});
