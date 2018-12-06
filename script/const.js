add35bd6a4d6c70dfbd069ff8c0977955e5e3363
murayama <tasuku.murayama@pactera.com>
2018-12-06 21:22:11 +0900

    ファイル名を変更

---
+++ script/const.js
@@ -0,0 +1,8 @@
+
+const TABLE_GUILD_WORD =  "guild/word/";
+
+const TABLE_GUILD_PROFILE =  "guild/profile/";
+
+const TABLE_GUILD_BATTLE =  "guild/battle/";
+
+const LOCALSTORAGE_KEY_GUILDBATTLE_DATE = "guildbattle"
--- script/firebase.js
+++
@@ -1,6 +0,0 @@
-
-const TABLE_GUILD_WORD =  "guild/word/";
-
-const TABLE_GUILD_LIST =  "guild/list/";
-
-const TABLE_GUILD_BATTLE =  "guild/battle/";
